3f819893989d5f2e8ad0182b211774bdad936e6e
Saloni Jariwala <salonijariwala.developing@gmail.com>
2022-10-19 12:02:08 +0530

    remove authorization

--- Controller/designation.js
+++ Controller/designation.js
@@ -3,30 +3,18 @@ const Designation = require("../Model/designation");
 const API_KEY = "geekwebsolution";
 
 const setDesignation = asyncHandler(async (req, res) => {
-    const { token } = req.query;
-    const compared = await bcrypt.compare(API_KEY, token);
-    if(compared) {
-        if (!req.body) {
-            res.status(400).send("Designation is missing");
-        }
-        const designation = await Designation.create({
-            name: req.body.name
-        });
-        res.status(200).json(designation);
-    } else {
-        res.status(400).send("Unauthorised Access");
+    if (!req.body) {
+        res.status(400).send("Designation is missing");
     }
+    const designation = await Designation.create({
+        name: req.body.name
+    });
+    res.status(200).json(designation);
 });
 
 const getDesignation = asyncHandler(async (req, res) => {
-    const { token } = req.query;
-    const compared = await bcrypt.compare(API_KEY, token);
-    if(compared) {
-        const allDesig = await Designation.find();
-        res.status(200).json(allDesig);
-    } else {
-        res.status(400).send("Unauthorised Access");
-    }
+    const allDesig = await Designation.find();
+    res.status(200).json(allDesig);
 });
 
 module.exports = { getDesignation, setDesignation };
--- Controller/userRole.js
+++ Controller/userRole.js
@@ -1,6 +1,5 @@
 const asyncHandler = require("express-async-handler");
 const UserRole = require("../Model/userRole");
-const API_KEY = "geekwebsolution";
 
 const setUserRole = asyncHandler(async (req, res) => {
     if (!req.body) {
@@ -13,13 +12,8 @@ const setUserRole = asyncHandler(async (req, res) => {
 });
 
 const getUserRole = asyncHandler(async (req, res) => {
-    const compared = await bcrypt.compare(API_KEY, req.query.token);
-    if(compared) {
-        const allUserRoles = await UserRole.find();
-        res.status(200).json(allUserRoles);
-    } else {
-        res.status(400).send("Unauthorised Access");
-    }
+    const allUserRoles = await UserRole.find();
+    res.status(200).json(allUserRoles);
 });
 
 module.exports = { getUserRole, setUserRole };
