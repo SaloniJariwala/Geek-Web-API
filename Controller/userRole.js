a40e378fe2816fdb8bf40b8dbeed813df6ebeadb
Saloni Jariwala <salonijariwala.developing@gmail.com>
2022-10-19 11:35:59 +0530

    authorization in userRole

--- Controller/userRole.js
+++ Controller/userRole.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const UserRole = require("../Model/userRole");
+const API_KEY = "geekwebsolution";
 
 const setUserRole = asyncHandler(async (req, res) => {
     if (!req.body) {
@@ -12,8 +13,13 @@ const setUserRole = asyncHandler(async (req, res) => {
 });
 
 const getUserRole = asyncHandler(async (req, res) => {
-    const allUserRoles = await UserRole.find();
-    res.status(200).json(allUserRoles);
+    const compared = await bcrypt.compare(API_KEY, req.query.token);
+    if(compared) {
+        const allUserRoles = await UserRole.find();
+        res.status(200).json(allUserRoles);
+    } else {
+        res.status(400).send("Unauthorised Access");
+    }
 });
 
 module.exports = { getUserRole, setUserRole };
